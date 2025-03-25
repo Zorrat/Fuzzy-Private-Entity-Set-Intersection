fd8787959bc065f095e0091984549f094f4efd93
Mohan Teja Guddimettla <gmtejar@gmail.com>
2025-03-25 18:57:27 +0000

    dataloader

--- data/data_test.go
+++ data/data_test.go
@@ -1,1 +1,18 @@
 package data
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoader(t *testing.T) {
+	loader := NewDataLoader()
+	data, err := loader.LoadDataFromTxt("data.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, 2, len(data))
+	assert.Equal(t, "Hello", data[0])
+	assert.Equal(t, "World!", data[1])
+}
--- data/dataloader.go
+++ data/dataloader.go
@@ -1,1 +1,36 @@
 package data
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type DataLoader struct {
+}
+
+func (d *DataLoader) LoadDataFromTxt(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// Preallocate memory for 10_000 lines
+	lines := make([]string, 0, 10_000)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return lines, nil
+}
+
+func NewDataLoader() *DataLoader {
+	return &DataLoader{}
+}
